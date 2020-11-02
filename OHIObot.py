55e0ea70ff1752cfea674cf62d7bdaabafbe8df6
Tushar <tas2124@gmail.com>
2020-11-01 21:32:17 -0600

    Initial Commit 3

--- OHIObot.py
+++ OHIObot.py
@@ -1,20 +1,21 @@
 import os
 
-import aiml
 from autocorrect import spell
 from flask import Flask, render_template, request
+
+import aiml_package
 
 app = Flask(__name__)
 
 BRAIN_FILE = "pretrained_model/pretrainedModel.dump"
-k = aiml.Kernel()
+k = aiml_package.Kernel()
 
 if os.path.exists(BRAIN_FILE):
     print("Loading from brain file: " + BRAIN_FILE)
     k.loadBrain(BRAIN_FILE)
 else:
-    print("Parsing aiml files")
-    k.bootstrap(learnFiles="pretrained_model/learningFileList.aiml", commands="load aiml")
+    print("Parsing aiml_package files")
+    k.bootstrap(learnFiles="pretrained_model/learningFileList.aiml_package", commands="load aiml_package")
     print("Saving brain file: " + BRAIN_FILE)
     k.saveBrain(BRAIN_FILE)
 
